d89d48f485134718c654a998a0583d4498bb4b7f
Harlan Valmer <hvalmer@gmail.com>
2020-03-26 11:38:22 -0300

    Solution with interface and abstract class

--- src/application/ProgramInterfaces2.java
+++ src/application/ProgramInterfaces2.java
@@ -1,16 +1,16 @@
 package application;
 
+import model.entities.AbstractShape;
 import model.entities.Circle;
 import model.entities.Rectangle;
-import model.entities.Shape;
 import model.enums.Color;
 
 public class ProgramInterfaces2 {
 
 	public static void main(String[] args) {
 	
-		Shape sh1 = new Circle(Color.BLACK, 2.0);
-		Shape sh2 = new Rectangle(Color.BLACK, 3.0, 4.0);
+		AbstractShape sh1 = new Circle(Color.BLACK, 2.0);
+		AbstractShape sh2 = new Rectangle(Color.BLACK, 3.0, 4.0);
 		
 		System.out.println("Circle color: " + sh1.getColor());
 		System.out.println("Circle area: " + String.format("%.3f", sh1.area()));
