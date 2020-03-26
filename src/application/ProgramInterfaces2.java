a4df970c4cce4260ebf6668f4df25a239e9b3929
Harlan Valmer <hvalmer@gmail.com>
2020-03-26 11:20:56 -0300

    Interface inicial

---
+++ src/application/ProgramInterfaces2.java
@@ -0,0 +1,20 @@
+package application;
+
+import model.entities.Circle;
+import model.entities.Rectangle;
+import model.entities.Shape;
+import model.enums.Color;
+
+public class ProgramInterfaces2 {
+
+	public static void main(String[] args) {
+	
+		Shape sh1 = new Circle(Color.BLACK, 2.0);
+		Shape sh2 = new Rectangle(Color.BLACK, 3.0, 4.0);
+		
+		System.out.println("Circle color: " + sh1.getColor());
+		System.out.println("Circle area: " + String.format("%.3f", sh1.area()));
+		System.out.println("Rectangle color: " + sh2.getColor());
+		System.out.println("Rectangle area: " + String.format("%.3f", sh2.area()));
+	}
+}
